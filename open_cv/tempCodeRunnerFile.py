3e04f37edb81b6d8ed7d1d51739897df62cd3bbb
panminhai <heiseinopan@gmail.com>
2023-02-28 15:37:53 +0800

    400*800_apple

--- open_cv/tempCodeRunnerFile.py
+++ open_cv/tempCodeRunnerFile.py
@@ -1,7 +1,13 @@
-'''
 import cv2
 
-image = cv2.imread('apple_soda.jpg')
-img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-'''
-print('hello world!')
+image = cv2.imread('C:/Users/e1356/python/open_cv/apple_soda.jpg')
+resize_img = cv2.resize(image, (400, 800))
+
+
+img_gray = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY)
+
+
+cv2.imshow('image_apple', img_gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
